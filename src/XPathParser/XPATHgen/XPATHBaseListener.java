52c041daa19aef6924d03cca2daf951881104a2d
LiuGQ1995 <isguoqiang.liu@gmail.com>
2020-02-02 14:05:23 -0800

    remove override signature

--- src/XPathParser/XPATHgen/XPATHBaseListener.java
+++ src/XPathParser/XPATHgen/XPATHBaseListener.java
@@ -16,312 +16,312 @@ public class XPATHBaseListener implements XPATHListener {
 	 *
 	 * <p>The default implementation does nothing.</p>
 	 */
-	@Override public void enterApSingle(XPATHParser.ApSingleContext ctx) { }
+	public void enterApSingle(XPATHParser.ApSingleContext ctx) { }
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation does nothing.</p>
 	 */
-	@Override public void exitApSingle(XPATHParser.ApSingleContext ctx) { }
+	public void exitApSingle(XPATHParser.ApSingleContext ctx) { }
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation does nothing.</p>
 	 */
-	@Override public void enterApDouble(XPATHParser.ApDoubleContext ctx) { }
+	public void enterApDouble(XPATHParser.ApDoubleContext ctx) { }
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation does nothing.</p>
 	 */
-	@Override public void exitApDouble(XPATHParser.ApDoubleContext ctx) { }
+	public void exitApDouble(XPATHParser.ApDoubleContext ctx) { }
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation does nothing.</p>
 	 */
-	@Override public void enterDoc(XPATHParser.DocContext ctx) { }
+	public void enterDoc(XPATHParser.DocContext ctx) { }
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation does nothing.</p>
 	 */
-	@Override public void exitDoc(XPATHParser.DocContext ctx) { }
+	public void exitDoc(XPATHParser.DocContext ctx) { }
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation does nothing.</p>
 	 */
-	@Override public void enterRpSelf(XPATHParser.RpSelfContext ctx) { }
+	public void enterRpSelf(XPATHParser.RpSelfContext ctx) { }
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation does nothing.</p>
 	 */
-	@Override public void exitRpSelf(XPATHParser.RpSelfContext ctx) { }
+	public void exitRpSelf(XPATHParser.RpSelfContext ctx) { }
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation does nothing.</p>
 	 */
-	@Override public void enterRpText(XPATHParser.RpTextContext ctx) { }
+	public void enterRpText(XPATHParser.RpTextContext ctx) { }
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation does nothing.</p>
 	 */
-	@Override public void exitRpText(XPATHParser.RpTextContext ctx) { }
+	public void exitRpText(XPATHParser.RpTextContext ctx) { }
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation does nothing.</p>
 	 */
-	@Override public void enterRpSingleSlash(XPATHParser.RpSingleSlashContext ctx) { }
+	public void enterRpSingleSlash(XPATHParser.RpSingleSlashContext ctx) { }
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation does nothing.</p>
 	 */
-	@Override public void exitRpSingleSlash(XPATHParser.RpSingleSlashContext ctx) { }
+	public void exitRpSingleSlash(XPATHParser.RpSingleSlashContext ctx) { }
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation does nothing.</p>
 	 */
-	@Override public void enterRpChildren(XPATHParser.RpChildrenContext ctx) { }
+	public void enterRpChildren(XPATHParser.RpChildrenContext ctx) { }
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation does nothing.</p>
 	 */
-	@Override public void exitRpChildren(XPATHParser.RpChildrenContext ctx) { }
+	public void exitRpChildren(XPATHParser.RpChildrenContext ctx) { }
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation does nothing.</p>
 	 */
-	@Override public void enterRpAttri(XPATHParser.RpAttriContext ctx) { }
+	public void enterRpAttri(XPATHParser.RpAttriContext ctx) { }
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation does nothing.</p>
 	 */
-	@Override public void exitRpAttri(XPATHParser.RpAttriContext ctx) { }
+	public void exitRpAttri(XPATHParser.RpAttriContext ctx) { }
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation does nothing.</p>
 	 */
-	@Override public void enterRpTag(XPATHParser.RpTagContext ctx) { }
+	public void enterRpTag(XPATHParser.RpTagContext ctx) { }
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation does nothing.</p>
 	 */
-	@Override public void exitRpTag(XPATHParser.RpTagContext ctx) { }
+	public void exitRpTag(XPATHParser.RpTagContext ctx) { }
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation does nothing.</p>
 	 */
-	@Override public void enterRpParent(XPATHParser.RpParentContext ctx) { }
+	public void enterRpParent(XPATHParser.RpParentContext ctx) { }
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation does nothing.</p>
 	 */
-	@Override public void exitRpParent(XPATHParser.RpParentContext ctx) { }
+	public void exitRpParent(XPATHParser.RpParentContext ctx) { }
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation does nothing.</p>
 	 */
-	@Override public void enterRpParentheses(XPATHParser.RpParenthesesContext ctx) { }
+	public void enterRpParentheses(XPATHParser.RpParenthesesContext ctx) { }
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation does nothing.</p>
 	 */
-	@Override public void exitRpParentheses(XPATHParser.RpParenthesesContext ctx) { }
+	public void exitRpParentheses(XPATHParser.RpParenthesesContext ctx) { }
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation does nothing.</p>
 	 */
-	@Override public void enterRpCondition(XPATHParser.RpConditionContext ctx) { }
+	public void enterRpCondition(XPATHParser.RpConditionContext ctx) { }
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation does nothing.</p>
 	 */
-	@Override public void exitRpCondition(XPATHParser.RpConditionContext ctx) { }
+	public void exitRpCondition(XPATHParser.RpConditionContext ctx) { }
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation does nothing.</p>
 	 */
-	@Override public void enterRpComma(XPATHParser.RpCommaContext ctx) { }
+	public void enterRpComma(XPATHParser.RpCommaContext ctx) { }
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation does nothing.</p>
 	 */
-	@Override public void exitRpComma(XPATHParser.RpCommaContext ctx) { }
+	public void exitRpComma(XPATHParser.RpCommaContext ctx) { }
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation does nothing.</p>
 	 */
-	@Override public void enterRpDoubleSlash(XPATHParser.RpDoubleSlashContext ctx) { }
+	public void enterRpDoubleSlash(XPATHParser.RpDoubleSlashContext ctx) { }
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation does nothing.</p>
 	 */
-	@Override public void exitRpDoubleSlash(XPATHParser.RpDoubleSlashContext ctx) { }
+	public void exitRpDoubleSlash(XPATHParser.RpDoubleSlashContext ctx) { }
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation does nothing.</p>
 	 */
-	@Override public void enterFilterIs(XPATHParser.FilterIsContext ctx) { }
+	public void enterFilterIs(XPATHParser.FilterIsContext ctx) { }
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation does nothing.</p>
 	 */
-	@Override public void exitFilterIs(XPATHParser.FilterIsContext ctx) { }
+	public void exitFilterIs(XPATHParser.FilterIsContext ctx) { }
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation does nothing.</p>
 	 */
-	@Override public void enterFilterRp(XPATHParser.FilterRpContext ctx) { }
+	public void enterFilterRp(XPATHParser.FilterRpContext ctx) { }
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation does nothing.</p>
 	 */
-	@Override public void exitFilterRp(XPATHParser.FilterRpContext ctx) { }
+	public void exitFilterRp(XPATHParser.FilterRpContext ctx) { }
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation does nothing.</p>
 	 */
-	@Override public void enterFilterParentheses(XPATHParser.FilterParenthesesContext ctx) { }
+	public void enterFilterParentheses(XPATHParser.FilterParenthesesContext ctx) { }
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation does nothing.</p>
 	 */
-	@Override public void exitFilterParentheses(XPATHParser.FilterParenthesesContext ctx) { }
+	public void exitFilterParentheses(XPATHParser.FilterParenthesesContext ctx) { }
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation does nothing.</p>
 	 */
-	@Override public void enterFilterEq(XPATHParser.FilterEqContext ctx) { }
+	public void enterFilterEq(XPATHParser.FilterEqContext ctx) { }
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation does nothing.</p>
 	 */
-	@Override public void exitFilterEq(XPATHParser.FilterEqContext ctx) { }
+	public void exitFilterEq(XPATHParser.FilterEqContext ctx) { }
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation does nothing.</p>
 	 */
-	@Override public void enterFilterOR(XPATHParser.FilterORContext ctx) { }
+	public void enterFilterOR(XPATHParser.FilterORContext ctx) { }
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation does nothing.</p>
 	 */
-	@Override public void exitFilterOR(XPATHParser.FilterORContext ctx) { }
+	public void exitFilterOR(XPATHParser.FilterORContext ctx) { }
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation does nothing.</p>
 	 */
-	@Override public void enterFilterAND(XPATHParser.FilterANDContext ctx) { }
+	public void enterFilterAND(XPATHParser.FilterANDContext ctx) { }
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation does nothing.</p>
 	 */
-	@Override public void exitFilterAND(XPATHParser.FilterANDContext ctx) { }
+	public void exitFilterAND(XPATHParser.FilterANDContext ctx) { }
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation does nothing.</p>
 	 */
-	@Override public void enterFilterNOT(XPATHParser.FilterNOTContext ctx) { }
+	public void enterFilterNOT(XPATHParser.FilterNOTContext ctx) { }
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation does nothing.</p>
 	 */
-	@Override public void exitFilterNOT(XPATHParser.FilterNOTContext ctx) { }
+	public void exitFilterNOT(XPATHParser.FilterNOTContext ctx) { }
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation does nothing.</p>
 	 */
-	@Override public void enterTagName(XPATHParser.TagNameContext ctx) { }
+	public void enterTagName(XPATHParser.TagNameContext ctx) { }
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation does nothing.</p>
 	 */
-	@Override public void exitTagName(XPATHParser.TagNameContext ctx) { }
+	public void exitTagName(XPATHParser.TagNameContext ctx) { }
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation does nothing.</p>
 	 */
-	@Override public void enterAttName(XPATHParser.AttNameContext ctx) { }
+	public void enterAttName(XPATHParser.AttNameContext ctx) { }
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation does nothing.</p>
 	 */
-	@Override public void exitAttName(XPATHParser.AttNameContext ctx) { }
+	public void exitAttName(XPATHParser.AttNameContext ctx) { }
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation does nothing.</p>
 	 */
-	@Override public void enterFilename(XPATHParser.FilenameContext ctx) { }
+	public void enterFilename(XPATHParser.FilenameContext ctx) { }
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation does nothing.</p>
 	 */
-	@Override public void exitFilename(XPATHParser.FilenameContext ctx) { }
+	public void exitFilename(XPATHParser.FilenameContext ctx) { }
 
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation does nothing.</p>
 	 */
-	@Override public void enterEveryRule(ParserRuleContext ctx) { }
+	public void enterEveryRule(ParserRuleContext ctx) { }
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation does nothing.</p>
 	 */
-	@Override public void exitEveryRule(ParserRuleContext ctx) { }
+	public void exitEveryRule(ParserRuleContext ctx) { }
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation does nothing.</p>
 	 */
-	@Override public void visitTerminal(TerminalNode node) { }
+	public void visitTerminal(TerminalNode node) { }
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation does nothing.</p>
 	 */
-	@Override public void visitErrorNode(ErrorNode node) { }
+	public void visitErrorNode(ErrorNode node) { }
 }
--- src/XPathParser/XPATHgen/XPATHBaseVisitor.java
+++ src/XPathParser/XPATHgen/XPATHBaseVisitor.java
@@ -17,166 +17,166 @@ public class XPATHBaseVisitor<T> extends AbstractParseTreeVisitor<T> implements 
 	 * <p>The default implementation returns the result of calling
 	 * {@link #visitChildren} on {@code ctx}.</p>
 	 */
-	@Override public T visitApSingle(XPATHParser.ApSingleContext ctx) { return visitChildren(ctx); }
+	public T visitApSingle(XPATHParser.ApSingleContext ctx) { return visitChildren(ctx); }
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation returns the result of calling
 	 * {@link #visitChildren} on {@code ctx}.</p>
 	 */
-	@Override public T visitApDouble(XPATHParser.ApDoubleContext ctx) { return visitChildren(ctx); }
+	public T visitApDouble(XPATHParser.ApDoubleContext ctx) { return visitChildren(ctx); }
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation returns the result of calling
 	 * {@link #visitChildren} on {@code ctx}.</p>
 	 */
-	@Override public T visitDoc(XPATHParser.DocContext ctx) { return visitChildren(ctx); }
+	public T visitDoc(XPATHParser.DocContext ctx) { return visitChildren(ctx); }
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation returns the result of calling
 	 * {@link #visitChildren} on {@code ctx}.</p>
 	 */
-	@Override public T visitRpSelf(XPATHParser.RpSelfContext ctx) { return visitChildren(ctx); }
+	public T visitRpSelf(XPATHParser.RpSelfContext ctx) { return visitChildren(ctx); }
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation returns the result of calling
 	 * {@link #visitChildren} on {@code ctx}.</p>
 	 */
-	@Override public T visitRpText(XPATHParser.RpTextContext ctx) { return visitChildren(ctx); }
+	public T visitRpText(XPATHParser.RpTextContext ctx) { return visitChildren(ctx); }
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation returns the result of calling
 	 * {@link #visitChildren} on {@code ctx}.</p>
 	 */
-	@Override public T visitRpSingleSlash(XPATHParser.RpSingleSlashContext ctx) { return visitChildren(ctx); }
+	public T visitRpSingleSlash(XPATHParser.RpSingleSlashContext ctx) { return visitChildren(ctx); }
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation returns the result of calling
 	 * {@link #visitChildren} on {@code ctx}.</p>
 	 */
-	@Override public T visitRpChildren(XPATHParser.RpChildrenContext ctx) { return visitChildren(ctx); }
+	public T visitRpChildren(XPATHParser.RpChildrenContext ctx) { return visitChildren(ctx); }
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation returns the result of calling
 	 * {@link #visitChildren} on {@code ctx}.</p>
 	 */
-	@Override public T visitRpAttri(XPATHParser.RpAttriContext ctx) { return visitChildren(ctx); }
+	public T visitRpAttri(XPATHParser.RpAttriContext ctx) { return visitChildren(ctx); }
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation returns the result of calling
 	 * {@link #visitChildren} on {@code ctx}.</p>
 	 */
-	@Override public T visitRpTag(XPATHParser.RpTagContext ctx) { return visitChildren(ctx); }
+	public T visitRpTag(XPATHParser.RpTagContext ctx) { return visitChildren(ctx); }
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation returns the result of calling
 	 * {@link #visitChildren} on {@code ctx}.</p>
 	 */
-	@Override public T visitRpParent(XPATHParser.RpParentContext ctx) { return visitChildren(ctx); }
+	public T visitRpParent(XPATHParser.RpParentContext ctx) { return visitChildren(ctx); }
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation returns the result of calling
 	 * {@link #visitChildren} on {@code ctx}.</p>
 	 */
-	@Override public T visitRpParentheses(XPATHParser.RpParenthesesContext ctx) { return visitChildren(ctx); }
+	public T visitRpParentheses(XPATHParser.RpParenthesesContext ctx) { return visitChildren(ctx); }
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation returns the result of calling
 	 * {@link #visitChildren} on {@code ctx}.</p>
 	 */
-	@Override public T visitRpCondition(XPATHParser.RpConditionContext ctx) { return visitChildren(ctx); }
+	public T visitRpCondition(XPATHParser.RpConditionContext ctx) { return visitChildren(ctx); }
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation returns the result of calling
 	 * {@link #visitChildren} on {@code ctx}.</p>
 	 */
-	@Override public T visitRpComma(XPATHParser.RpCommaContext ctx) { return visitChildren(ctx); }
+	public T visitRpComma(XPATHParser.RpCommaContext ctx) { return visitChildren(ctx); }
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation returns the result of calling
 	 * {@link #visitChildren} on {@code ctx}.</p>
 	 */
-	@Override public T visitRpDoubleSlash(XPATHParser.RpDoubleSlashContext ctx) { return visitChildren(ctx); }
+	public T visitRpDoubleSlash(XPATHParser.RpDoubleSlashContext ctx) { return visitChildren(ctx); }
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation returns the result of calling
 	 * {@link #visitChildren} on {@code ctx}.</p>
 	 */
-	@Override public T visitFilterIs(XPATHParser.FilterIsContext ctx) { return visitChildren(ctx); }
+	public T visitFilterIs(XPATHParser.FilterIsContext ctx) { return visitChildren(ctx); }
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation returns the result of calling
 	 * {@link #visitChildren} on {@code ctx}.</p>
 	 */
-	@Override public T visitFilterRp(XPATHParser.FilterRpContext ctx) { return visitChildren(ctx); }
+	public T visitFilterRp(XPATHParser.FilterRpContext ctx) { return visitChildren(ctx); }
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation returns the result of calling
 	 * {@link #visitChildren} on {@code ctx}.</p>
 	 */
-	@Override public T visitFilterParentheses(XPATHParser.FilterParenthesesContext ctx) { return visitChildren(ctx); }
+	public T visitFilterParentheses(XPATHParser.FilterParenthesesContext ctx) { return visitChildren(ctx); }
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation returns the result of calling
 	 * {@link #visitChildren} on {@code ctx}.</p>
 	 */
-	@Override public T visitFilterEq(XPATHParser.FilterEqContext ctx) { return visitChildren(ctx); }
+	public T visitFilterEq(XPATHParser.FilterEqContext ctx) { return visitChildren(ctx); }
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation returns the result of calling
 	 * {@link #visitChildren} on {@code ctx}.</p>
 	 */
-	@Override public T visitFilterOR(XPATHParser.FilterORContext ctx) { return visitChildren(ctx); }
+	public T visitFilterOR(XPATHParser.FilterORContext ctx) { return visitChildren(ctx); }
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation returns the result of calling
 	 * {@link #visitChildren} on {@code ctx}.</p>
 	 */
-	@Override public T visitFilterAND(XPATHParser.FilterANDContext ctx) { return visitChildren(ctx); }
+	public T visitFilterAND(XPATHParser.FilterANDContext ctx) { return visitChildren(ctx); }
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation returns the result of calling
 	 * {@link #visitChildren} on {@code ctx}.</p>
 	 */
-	@Override public T visitFilterNOT(XPATHParser.FilterNOTContext ctx) { return visitChildren(ctx); }
+	public T visitFilterNOT(XPATHParser.FilterNOTContext ctx) { return visitChildren(ctx); }
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation returns the result of calling
 	 * {@link #visitChildren} on {@code ctx}.</p>
 	 */
-	@Override public T visitTagName(XPATHParser.TagNameContext ctx) { return visitChildren(ctx); }
+	public T visitTagName(XPATHParser.TagNameContext ctx) { return visitChildren(ctx); }
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation returns the result of calling
 	 * {@link #visitChildren} on {@code ctx}.</p>
 	 */
-	@Override public T visitAttName(XPATHParser.AttNameContext ctx) { return visitChildren(ctx); }
+	public T visitAttName(XPATHParser.AttNameContext ctx) { return visitChildren(ctx); }
 	/**
 	 * {@inheritDoc}
 	 *
 	 * <p>The default implementation returns the result of calling
 	 * {@link #visitChildren} on {@code ctx}.</p>
 	 */
-	@Override public T visitFilename(XPATHParser.FilenameContext ctx) { return visitChildren(ctx); }
+	public T visitFilename(XPATHParser.FilenameContext ctx) { return visitChildren(ctx); }
 }
